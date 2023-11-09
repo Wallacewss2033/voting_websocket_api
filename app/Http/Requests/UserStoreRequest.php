299dc244409f83ec2800668e006c3c7aaedc0bed
Wallacewss2033 <wallace-2033@hotmail.com>
2023-11-09 18:41:12 -0300

    request user

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'email.required' => 'O campo e-mail é obrigatório.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'email.unique' => 'Este e-mail já está sendo ultilizado.',
+        ];
+    }
+}
